b7d97f00a5a4a4a165b97ed721e8936927041e68
Li Jinyao <lijinyao@outlook.com>
2016-09-05 23:33:52 +0800

    init project

---
+++ naiveDB/naiveDB/naiveDB.cpp
@@ -0,0 +1,49 @@
+// naiveDB.cpp : Defines the entry point for the console application.
+//
+#pragma once
+#include "stdafx.h"
+#include "Grammar.hpp"
+
+int main() {
+	using boost::spirit::standard_wide::space;
+	typedef std::wstring::const_iterator iterator_type;
+	typedef naiveDB::parser::SQLRule<iterator_type> SQLRule;
+	SQLRule g; // Our grammar
+	std::wstring str;
+	std::wstring logo = L"\
+              _           _____  ____  \n\
+             (_)         |  __ \\|  _ \\ \n\
+  _ __   __ _ ___   _____| |  | | |_) |\n\
+ | '_ \\ / _` | \\ \\ / / _ \\ |  | |  _ < \n\
+ | | | | (_| | |\\ V /  __/ |__| | |_) |\n\
+ |_| |_|\\__,_|_| \\_/ \\___|_____/|____/ \n\
+Welcome to naiveDB!";
+	std::wcout << logo << std::endl;
+	while (getline(std::wcin, str)) {
+		if (str.empty() || str[0] == 'q' || str[0] == 'Q') {
+			break;
+		}
+		naiveDB::parser::TopSQLStatement ste;
+		std::wstring::const_iterator iter = str.begin();
+		std::wstring::const_iterator end = str.end();
+		bool r = phrase_parse(iter, end, g, space, ste);
+
+		if (r && iter == end) {
+
+			//std::cout << boost::fusion::tuple_open('[');
+			//std::cout << boost::fusion::tuple_close(']');
+			//std::cout << boost::fusion::tuple_delimiter(", ");
+
+			std::wcout << "Parsing succeeded\n";
+			boost::apply_visitor(naiveDB::parser::SQLparser(), ste.sql);
+		}
+		else {
+			std::wcout << "Parsing failed\n";
+			std::wstring rest(iter, end);
+			std::wcout << "stopped at: " << rest << std::endl;
+		}
+
+	}
+	return 0;
+}
+
---
+++ naiveDB/naiveDB/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+
+// TODO: reference additional headers your program requires here
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/support_standard_wide.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/include/qi_optional.hpp>
+#include <boost/spirit/include/qi_attr.hpp>
+#include <boost/spirit/include/qi_uint.hpp>
